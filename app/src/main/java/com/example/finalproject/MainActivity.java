eabf0ea5be62e3bf0264527d697cd69ad796006f
Khánh Trình Ngô <trinhtit2003@gmail.com>
2023-10-08 02:07:28 +0700

    Sceen 1, 2

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Screen2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
