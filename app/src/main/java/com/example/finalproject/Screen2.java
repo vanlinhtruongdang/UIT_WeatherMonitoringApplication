249b1f8b84da87e8fc56d2d1f831bcce6dbf2b8a
Khánh Trình Ngô <trinhtit2003@gmail.com>
2023-11-07 00:17:58 +0700

    add fname, lname, back button

--- app/src/main/java/com/example/finalproject/Screen2.java
+++ app/src/main/java/com/example/finalproject/Screen2.java
@@ -2,6 +2,7 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,15 +15,10 @@ public class Screen2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
     }
-    public void signUpClicked(View view) {
-        EditText et_password = findViewById(R.id.et_password);
-        String password = et_password.getText().toString();
-        EditText et_cf = findViewById(R.id.et_cf_pass);
-        String confirmPassword = et_cf.getText().toString();
-        if (password.equals(confirmPassword)) {
-            Toast.makeText(this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Mật khẩu không khớp!", Toast.LENGTH_SHORT).show();
-        }
+
+    public void backClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/finalproject/Screen3.java
+++ app/src/main/java/com/example/finalproject/Screen3.java
@@ -2,7 +2,11 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Screen3 extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class Screen3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen3);
+    }
+    public void backClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
